8c87bf7f634c1ff867398cce852b87ec592de0bf
Rarer, John Christopher <jackrarer@gmail.com>
2024-04-23 20:17:19 -0400

    Cursor Updates

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -5,25 +5,44 @@ using UnityEngine.Animations;
 
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Interactable : MonoBehaviour
 {
     private bool hovering;
     private bool interacted;
+
+
+    private SpriteRenderer sr;
+    private Sprite normalSprite;
+    [SerializeField] private Texture2D hoverCursorTexture;
+    [SerializeField] private Sprite hoverSprite;
+
     [SerializeField] private List<Interactable> prereqs;
     Animator anim;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        normalSprite = sr.sprite;
     }
     private void OnMouseExit()
     {
         hovering = false;
+        sr.sprite = normalSprite;
+
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 
     private void OnMouseEnter()
     {
         hovering = true;
+        
+        if (!interacted && CheckInteraction())
+        {
+            sr.sprite = hoverSprite;
+            Cursor.SetCursor(hoverCursorTexture, Vector2.zero, CursorMode.Auto); // Set custom cursor
+        }
     }
 
     private void OnMouseDown()
@@ -58,6 +77,9 @@ public class Interactable : MonoBehaviour
         interacted = true;
 
         anim.SetBool("interact", true);
+
+        sr.sprite = normalSprite;
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 
     private void FailInteract()
