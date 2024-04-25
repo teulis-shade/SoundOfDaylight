f240d3b8bace62a2f76ac8d8c4618e9d639cd87b
Teulis <tziegler30@gatech.edu>
2024-04-25 16:23:45 -0400

    Title Screen

---
+++ Assets/Scripts/TitleControl.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleControl : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void Credits()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
